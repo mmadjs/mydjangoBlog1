070917d3fdf1e0fa295a0ba482c6ece5bf08e1d4
mmadjs <mmad1999js@gmail.com>
2020-10-01 22:57:21 +0330

    urls

--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .import views
 
+app_name="articles"
 urlpatterns=[
 
-path('',views.articles_list),
+path('',views.articles_list,name=" list"),
+path('<slug>',views.articles_detail,name="detail"),
 
 
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render , HttpResponse
 from .import models
 
 # Create your views here.
 def articles_list(request):
     articles = models.Article.objects.all().order_by('date')
     return render(request,'articles/articleslist.html',{'articles':articles})
-    
+
+
+def articles_detail(request,slug):
+    return HttpResponse(slug)
